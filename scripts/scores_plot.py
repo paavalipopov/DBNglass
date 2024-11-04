e4e16adaacbe5358f9d4dcd92b55d43656b622cc
Pavel Popov <kapssula@gmail.com>
2024-11-04 19:14:11 +0000

    updated the AUROC quick plotting script

--- scripts/scores_plot.py
+++ scripts/scores_plot.py
@@ -21,6 +21,7 @@ paths[f"meanMLP"] = f"/data/users2/ppopov1/mlp-project/assets/logs/NEW_ICA-exp-m
 paths[f"glassDBN"] = f"/data/users2/ppopov1/glass_proj/assets/logs/new_ica-exp-DBNglassDeeper_defHP-fbirn/runs.csv"
 paths[f"glassDBN rerun"] = f"/data/users2/ppopov1/glass_proj/assets/logs/_orig-exp-DBNglassDeeper_defHP-fbirn/runs.csv"
 paths[f"glassDBN fixed"] = f"/data/users2/ppopov1/glass_proj/assets/logs/_cleaned-exp-DBNglassFIX_defHP-fbirn/runs.csv"
+paths[f"glassDBN denser"] = f"/data/users2/ppopov1/glass_proj/assets/logs/_denser-exp-DBNglassFIX_defHP-fbirn/runs.csv"
 
 # List to store the data for plotting
 data_list = []
@@ -43,28 +44,8 @@ plt.title('Test Score Distribution by Model')
 plt.ylabel('AUROC')
 plt.xlabel('Model')
 
-# # List to store the data for plotting
-# data_list = []
-
-# # Read each CSV file and store the relevant data
-# for model_name, path in paths.items():
-#     df = pd.read_csv(path)
-#     # Assuming the CSV file has a column named "test_score"
-#     df['model'] = model_name  # Add a column with the model name
-#     data_list.append(df[['bsnip_score', 'model']])
-
-# # Concatenate all the data into a single DataFrame
-# data = pd.concat(data_list)
-
-# # Plot the data using seaborn
-# plt.figure(figsize=(26, 6))
-# sns.boxplot(x='model', y='bsnip_score', data=data)
-# # plt.ylim(0.7, 0.95)
-# plt.title('BSNIP Score Distribution by Model')
-# plt.ylabel('AUROC')
-# plt.xlabel('Model')
 
 # Show the plot
-plt.savefig(f"/data/users2/ppopov1/glass_proj/scripts/pictures/model_comparison_{UTCNOW}.png")
+plt.savefig(f"/data/users2/ppopov1/glass_proj/scripts/pictures2/model_comparison_{UTCNOW}.png")
 
 
